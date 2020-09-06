9156c1a75957c5596bf0d3326bb2b810df134d8d
ZhouLan <1832311982@qq.com>
2020-09-06 10:45:33 +0800

    子串判断

---
+++ contents/string/src/$1.java
@@ -0,0 +1,10 @@
+public class $1 {
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        boolean[] res = new boolean[n];
+        for (int i = 0; i < p.length; i++) {
+            res[i] = s.contains(p[i]);
+        }
+        return res;
+    }
+    
+}
